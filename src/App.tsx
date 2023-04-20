2c814c77f1d66db4ae6ea9cad25f7373e9b0e271
Waldemar Głaz <w.glaz@cheil.com>
2023-04-20 10:51:48 +0200

    routing issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -39,12 +39,15 @@ function App() {
         <Route index element={<Home />} />
         {/* ----- TODO render dynamically ----- */}
         <Route path="destinations" element={<Destinations data={data.destinations} />}>
+          <Route index element={<Navigate to="Moon" />} />
           <Route path=":destination" element={<Destination data={data.destinations} />} />
         </Route>
         <Route path="crew" element={<Crew data={data.crew} />}>
+          <Route index element={<Navigate to="Victor Glover" />} />
           <Route path=":crewMember" element={<CrewMember crewData={data.crew} />} />
         </Route>
         <Route path="technology" element={<Technology data={data.technology} />}>
+          <Route index element={<Navigate to="Spaceport" />} />
           <Route path=":technologyName" element={<TechnologyName technologyData={data.technology} />} />
         </Route>
         <Route path="*" element={<NoMatch />} />
