cff59541ffb923fb35034624f5b5352c8c2bbc79
Waldemar Głaz <w.glaz@cheil.com>
2023-04-24 21:33:42 +0200

    HomePage styles

--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Header = (props) => {
-  return <header className="header">{props.children}</header>;
-};
-
-export default Header;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,16 +3,18 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div className="page home-page">
-      <div className="home-page__wrapper">
-        <div className="eyebrow">SO, YOU WANT TO TRAVEL TO</div>
-        <div className="page__title page__title--large">SPACE</div>
-        <p className="page__text">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    <main>
+      <div className="page home-page">
+        <div className="home-page__wrapper">
+          <div className="eyebrow">SO, YOU WANT TO TRAVEL TO</div>
+          <div className="page__title page__title--large">SPACE</div>
+          <p className="page__text">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
+        <Link className="home-page__cta" to="/destinations/Mars">
+          EXPLORE
+        </Link>
       </div>
-      <Link className="home-page__cta" to="/destinations/Mars">
-        EXPLORE
-      </Link>
-    </div>
+    </main>
   );
 };
 
